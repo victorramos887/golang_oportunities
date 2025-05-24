c6a2765a8141852755d62591550a11a226b8c548
Victor Ramos <v.ramos58@hotmail.com>
2025-05-24 13:09:11 -0300

    feat: update handler structs set package

--- router/routes.go
+++ router/routes.go
@@ -1,34 +1,20 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/victorramos887/golang_oportunities/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 
-	v1.GET("/opening", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "GET Opening"})
-	})
+	v1.GET("/opening", handler.ShowOpeningHandler)
 
-	v1.POST("/opening", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "POST Opening"})
-	})
+	v1.POST("/opening", handler.CreateOpeningHandler)
 
-	v1.DELETE("/opening", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "DELETE Opening"})
-	})
+	v1.DELETE("/opening", handler.DeleteOpeningHandler)
 
-	v1.PUT("/opening", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "PUT Opening"})
-	})
+	v1.PUT("/opening", handler.UpdateOpeningHandler)
 
-	v1.GET("/openings", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "PUT Opening"})
-	})
-
-	// Adicione outras rotas aqui, por exemplo:
-	// v1.GET("/outra-rota", handlerFunc)
+	v1.GET("/openings", handler.ShowAllOpeningHandler)
 }
