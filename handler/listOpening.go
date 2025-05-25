4cf3772638972c13354a28db037830ad508d11db
Victor Ramos <v.ramos58@hotmail.com>
2025-05-25 17:15:06 -0300

    feat: show only record or show list records

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -4,9 +4,17 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/victorramos887/golang_oportunities/schemas"
 )
 
 func ListOpeningHandler(ctx *gin.Context) {
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "GET Openings"})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,12 +1,29 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/victorramos887/golang_oportunities/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "GET Opening"})
+	//ctx.JSON(http.StatusOK, gin.H{"message": "GET Opening"})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "get-opening", opening)
 }
