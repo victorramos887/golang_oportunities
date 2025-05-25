fa0949119b8938e6aab622b9472ab2c2fe83f0a3
Victor Ramos <v.ramos58@hotmail.com>
2025-05-25 18:13:03 -0300

    feat: add update opening

--- handler/request.go
+++ handler/request.go
@@ -45,3 +45,24 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+
+	// if none of the fileds were provider return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+}
